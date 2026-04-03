8b22b4e8d366cc87cf7f2710be05f6417fb3166e
gulshanratan351-rgb <gulshanratan351@gmail.com>
2026-04-03 17:44:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from aiohttp import web
 API_ID = int(os.environ.get("API_ID", "0"))
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-MONGO_URL = os.environ.get("MONGO_URL", "")
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://radhika8954994958_db_user:FJGjPO5qYLIFTWRO@cluster0.nbqdj5l.mongodb.net/?appName=Cluster0")
 CHANNEL = os.environ.get("CHANNEL_USERNAME", "")
 ADMIN = int(os.environ.get("ADMIN_ID", "0"))
 PORT = int(os.environ.get("PORT", 8080))
